0dfbec3a058da6d5bfca574608bedf473ebbb74c
Nadia Kravina <nadiakravina@gmail.com>
2024-06-03 14:22:56 +0000

    Set up structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame () {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function inctrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubstractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
